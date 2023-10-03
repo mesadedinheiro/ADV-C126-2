6ed51c1b41221e5330a72369e0d204a73aec815b
mesadedinheiro <139721799+mesadedinheiro@users.noreply.github.com>
2023-10-02 21:44:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song1 = ""
+song2 = ""
+function preload() {
+    song = loadSound("music2.mp3")
+}
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide;
+}
+function draw() {
+    Image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+}
